369433b42e14ebc91a9bbce34d0e574cb27e439c
KiranRiz <146770877+KiranRiz@users.noreply.github.com>
2026-01-26 08:53:46 +0000

    code change

--- test.js
+++ test.js
@@ -1,1 +1,18 @@
-alert("Hello World!");
+alert("Hello World!");function sumM(f, m) {
+    let sum = 0;
+
+    for (let i = 0; i < m.length; i++) {
+        for (let j = 0; j < f.length; j++) {
+            if (m[i] % f[j] == 0) {
+                sum += m[i];
+            }
+        }
+    }
+
+    return sum;
+}
+
+let f = [2, 3];
+let m = [2, 3, 4, 5, 6, 7, 8];
+
+console.log("Sum of multiples:", sumM(f, m));
