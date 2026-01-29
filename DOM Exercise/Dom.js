0f77ffea7e956443a63f4bcf1e12f8702bc90c6b
KiranRiz <146770877+KiranRiz@users.noreply.github.com>
2026-01-29 13:55:49 +0000

    now run

--- DOM Exercise/Dom.js
+++ DOM Exercise/Dom.js
@@ -1,10 +1,8 @@
 function Action() {
-
     let name = document.getElementById("text1").value;
     let age = document.getElementById("text2").value;
 
-    let output = "your" + name + "and age is" + age;
+    let output = "Your " + name + ", age " + age;
 
     document.getElementById("result").innerHTML = output;
-
 }
