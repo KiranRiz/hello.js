aaffed966145494245385c5e2b8ffee8b79af8ec
KiranRiz <146770877+KiranRiz@users.noreply.github.com>
2026-01-29 14:13:22 +0000

    js linked

--- DOM Exercise/Dom.js
+++ DOM Exercise/Dom.js
@@ -2,7 +2,7 @@ function Action() {
     let name = document.getElementById("text1").value;
     let age = document.getElementById("text2").value;
 
-    let output = "Your " + name + ", age " + age;
+    let output = "Your Details " + name + ", age " + age;
 
     document.getElementById("result").innerHTML = output;
 }
