fb2f722587cb2ce547fe32c0f7bf4dc47a0d6c1a
KiranRiz <146770877+KiranRiz@users.noreply.github.com>
2026-02-03 14:44:24 +0000

    API fetch

---
+++ API_Fetch/script.js
@@ -0,0 +1,11 @@
+let url = "https://jsonplaceholder.typicode.com/users";
+
+fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    let div = document.getElementById("data");
+
+    data.forEach(user => {
+      div.innerHTML += user.name + "<br>";
+    });
+  });
